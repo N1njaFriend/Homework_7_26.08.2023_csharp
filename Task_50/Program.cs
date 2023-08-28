7daf334a0d550d309772ba66a770cf35edb69523
eN1nja <mr.buntar@gmail.com>
2023-08-28 16:19:10 +0300

    first commit

---
+++ Task_50/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int n = 5;
+int m = 5;
+int[,] arr = new int[n, m];
+Random random = new Random();
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        arr[i, j] = random.Next(10, 99);
+        Console.Write("{0} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите строку: ");
+int rows = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int columns = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (rows < 0 || rows >= n || columns < 0 || columns >= m)
+{
+    Console.WriteLine("Элемент не существует");
+}
+else
+{
+        Console.WriteLine($"Значение элемента массива = {arr[rows, columns]}");
+}
---
+++ Task_52/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < columns; j++)
+{
+    int sumNum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        sumNum += matrix[i, j];
+    }
+    Console.WriteLine($"Сумма столбца {j + 1} = {sumNum}");
+}
